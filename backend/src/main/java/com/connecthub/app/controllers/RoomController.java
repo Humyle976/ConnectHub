f258406693df2bc8db54cf35618d217f295366cd
Humyle976 <shumylem21@gmail.com>
2025-10-26 12:51:09 +0500

    format api response

--- backend/src/main/java/com/connecthub/app/controllers/RoomController.java
+++ backend/src/main/java/com/connecthub/app/controllers/RoomController.java
@@ -3,9 +3,11 @@ package com.connecthub.app.controllers;
 import com.connecthub.app.models.RoomModel;
 import com.connecthub.app.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RoomController {
@@ -14,22 +16,22 @@ public class RoomController {
     private RoomService roomService;
 
     @GetMapping("/rooms")
-    public List<RoomModel> getRooms() {
+    public ResponseEntity<Map<String,Object>> getRooms() {
         return roomService.getRooms();
     }
 
     @PostMapping("/room")
-    public RoomModel createRoom(@RequestBody RoomModel roomModel) {
+    public ResponseEntity<Map<String,Object>> createRoom(@RequestBody RoomModel roomModel) {
         return roomService.createRoom(roomModel);
     }
 
     @DeleteMapping("/room/{roomId}")
-    public void deleteRoom(@PathVariable("roomId") Integer roomId) {
-        roomService.deleteRoomById(roomId);
+    public ResponseEntity<Map<String,Object>> deleteRoom(@PathVariable("roomId") Integer roomId) {
+        return roomService.deleteRoomById(roomId);
     }
 
     @GetMapping("/room/{roomId}")
-    public RoomModel getRoom(@PathVariable("roomId") Integer roomId) {
+    public ResponseEntity<Map<String,Object>> getRoom(@PathVariable("roomId") Integer roomId) {
         return roomService.getRoomById(roomId);
     }
 }
